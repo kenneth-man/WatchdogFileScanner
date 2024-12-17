d523519ac807eeecbd3570a044b984a14ef9b54d
Kenneth Man <kenneth.waikin.man@outlook.com>
2024-12-17 22:03:53 +0000

    update examples

--- testingAsyncioAiohttp.py
+++ testingAsyncioAiohttp.py
@@ -18,21 +18,22 @@ async def main():
 	##############
 	# Example 1...
 	##############
-		# Tasks are used to run coroutines concurrently
-	# task1 = asyncio.create_task(getFetch("https://google.com"))
-	# task2 = asyncio.create_task(getFetch("https://linkedin.com"))
-	# task3 = asyncio.create_task(getFetch("https://facebook.com"))
+	# Tasks are used to run coroutines concurrently
+	task1 = asyncio.create_task(getFetch("https://google.com"))
+	task2 = asyncio.create_task(getFetch("https://linkedin.com"))
+	task3 = asyncio.create_task(getFetch("https://facebook.com"))
 
-	# result1 = await task1
-	# result2 = await task2
-	# result3 = await task3
+	result1 = await task1
+	result2 = await task2
+	result3 = await task3
 
-	# print(result1, "\n", result2, "\n", result3)
+	print(result1, "\n", result2, "\n", result3)
+
 
 	##############
 	# Example 2...
 	##############
-		# More concise way using 'asyncio.gather()'
+	# # More concise way using 'asyncio.gather()'
 	# results = await asyncio.gather(
 	# 	getFetch("https://google.com"),
 	# 	getFetch("https://linkedin.com"),
@@ -46,24 +47,24 @@ async def main():
 	##############
 	# Example 3...
 	##############
-		# Another way is to use 'asyncio.TaskGroup()' which allows for error handling
-	urls = [
-		"https://google.com",
-		"https://linkedin.com",
-		"https://facebook.com"
-	]
-	tasks = []
+	# # Another way is to use 'asyncio.TaskGroup()' which allows for error handling
+	# urls = [
+	# 	"https://google.com",
+	# 	"https://linkedin.com",
+	# 	"https://facebook.com"
+	# ]
+	# tasks = []
 
-	async with asyncio.TaskGroup() as taskGroup:
-		for u in urls:
-			task = taskGroup.create_task(getFetch(u))
-			tasks.append(task)
+	# async with asyncio.TaskGroup() as taskGroup:
+	# 	for u in urls:
+	# 		task = taskGroup.create_task(getFetch(u))
+	# 		tasks.append(task)
 	
-	# After the Task Group block, all tasks have finished
-	results = [t.result() for t in tasks]
+	# # After the Task Group block, all tasks have finished
+	# results = [t.result() for t in tasks]
 
-	for r in results:
-		print(r)
+	# for r in results:
+	# 	print(r)
 
 if __name__ == "__main__":
 	asyncio.run(main())
